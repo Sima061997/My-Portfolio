9884e91fc1fb69db06a7f6e167f9f74b52c9bf50
Sima Dhimal <sima.dhimal97@gmail.com>
2022-05-23 16:50:24 +0200

    finished with Portfolio page

--- index.js
+++ index.js
@@ -11,32 +11,3 @@ $(function () {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
  });
-
- //sider navigation bar on small screen size
-document.getElementsByClassName("button")[0].addEventListener("click", event => {
-    document.getElementById("navbarText").style.width = "300px";
-})
-
-function closeNav() {
-  document.getElementById("navbarText").style.width = "0";
-
-}
-
-/*
-// more contents on click of screen image 
-document.querySelectorAll(".grid-item").forEach(item => {
-  item.addEventListener("click", event => {
-    console.log("content item clicked");
-  const removeItem = document.getElementsByName("front-image");
-  removeItem.remove();
-    const moreImages = document.createElement("img");
-    moreImages.src = "img/image1.png";
-    document.body.appendChild(moreImages);
-
-    /*
-    const moreContents = event.target.getElementsByClassName(".onclick");
-    event.preventDefault();
-
-  })
-})
-*/ 
