7e2c7e15fba7f3f579706606d23e995dbd17e955
unknown <sima.dhimal2@stud.uni-hannover.de>
2024-10-03 19:36:06 +0200

    Portfolio

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-//color and height of navigation bar on scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
 
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > nav.offsetHeight) {
+          nav.classList.add("scrolled");
+      } else {
+          nav.classList.remove("scrolled");
+      }
   });
-  
-//scroll the page to the top page on click of top button
-
-  $("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
- });
+});
